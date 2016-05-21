ba446680474dd8f3c302e01eb65cdb8f1c305286
Beligh HAMDI <beligh.hamdi@influans.com>
2016-05-21 21:49:11 +0100

    fix some formType styles

--- src/AppBundle/Form/PurchaseType.php
+++ src/AppBundle/Form/PurchaseType.php
@@ -6,6 +6,11 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+
 class PurchaseType extends AbstractType
 {
     /**
@@ -15,10 +20,36 @@ class PurchaseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('start_date', 'date')
-            ->add('end_date', 'date')
+
+            ->add('title', TextType::class, array(
+                    'label' => 'Title',
+                    'attr'  => array('class' => 'form-control')
+                )
+            )
+
+            ->add('description', TextareaType::class, array(
+                    'label' => 'Description',
+                    'attr'  => array('class' => 'form-control')
+                )
+            )
+
+            ->add('start_date', DateType::class, array(
+                    'label' => 'Start date',
+                    'attr'  => array('class' => 'form-control')
+                )
+            )
+
+            ->add('end_date', DateType::class, array(
+                    'label' => 'End date',
+                    'attr'  => array('class' => 'form-control')
+                )
+            )
+
+
+          //  ->add('title')
+          //  ->add('description')
+          //  ->add('start_date', 'date')
+          //  ->add('end_date', 'date')
         ;
     }
     
